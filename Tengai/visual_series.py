c651ff7158b4dec1455f3e8907e647d7f28ce8e6
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-10-09 23:16:55 +0700

    feat: data

--- Tengai/visual_series.py
+++ Tengai/visual_series.py
@@ -67,43 +67,46 @@ def lineplot_resample_feature(data: pd.DataFrame, on_feat: str, rule: str) -> No
         resample_data = pd.Series(data=resample_data.values[:, 0], name=resample_data.columns[0])
         lineplot_features(resample_data)
         
+
+
+def average_generating_barplot(data: pd.DataFrame, on_feat: str, size: tuple[int] = (12, 6)):
+    """
+    Generate a bar plot of the mean values of numeric features grouped by the hour component of on_feat.
+    Highlight the bar with the maximum value in red.
+    Args:
+        data (pd.DataFrame): The input DataFrame.
+        on_feat (str): The feature used for grouping and x-axis labels.
+        size (tuple[int]): The size of the plot.
+    """
+    # Filter numeric columns
+    numeric_feat = data.select_dtypes(np.number).columns
     
-
-def avarange_genarating_barplot(data:pd.DataFrame,on_feat:str):
-    # how to mean in Dataframe order by on_feat
-    numeric_feat = data.columns.drop(on_feat)
+    # Calculate the mean values and reset the index
     mean_data = data.groupby(data[on_feat].dt.hour)[numeric_feat].mean().reset_index()
-    # setting figure
-    n_features = len(data.columns) - 1
-    n_col = min(n_features, 2)
-    n_row = n_features // (n_col)
+    
+    if mean_data.shape[1] > 1:
+        # value max in idx
+        max_idx = mean_data[numeric_feat[0]].idxmax()
+        colors = ['blue'] * len(mean_data)
+        # setting color of the maximum value bar by index
+        colors[max_idx] = 'red'
+        _ = plt.figure(figsize=size)
+        plt.bar(mean_data[on_feat].astype(str), mean_data[numeric_feat[0]])
+        plt.xlabel(on_feat)
+        plt.ylabel(f"Mean of {numeric_feat[0]}")
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+        plt.xticks(rotation=45)  
 
-    fig, ax = plt.subplots(n_row + 1, n_col, figsize=(20, 10))
+        # Find the index of the maximum value
+        max_idx = mean_data[numeric_feat[0]].idxmax()
 
-    for i, feat in enumerate(numeric_feat):
-        ax[i // 2, i % 2].bar(mean_data[on_feat].astype(str), mean_data[feat])
-        # Find maximum value and its index
-        max_val = mean_data[feat].max()
-        max_idx = mean_data[mean_data[feat] == max_val].index[0]  # Use .index[0] to get the first occurrence
-        rects = ax[i // 2, i % 2].containers[0]
-        
-        for j, rect in enumerate(rects):
-            if j == max_idx:
-                rect.set_color('red')
-            else:
-                rect.set_color('skyblue')
-            ax[i // 2, i % 2].annotate(f'{mean_data[feat][j]:.2f}', 
-                                       xy=(rect.get_x() + rect.get_width() / 2, rect.get_height()),
-                                       xytext=(0, 3), textcoords='offset points', ha='center', va='bottom',
-                                       color='black')
-        ax[i // 2, i % 2].set_xlabel(on_feat)
-        ax[i // 2, i % 2].set_ylabel(f"Mean of {feat}")
-        ax[i // 2, i % 2].grid(axis='y', linestyle='--', alpha=0.7)
-    # Turn off empty subplots
-    for i in range(n_features, n_row * n_col):
-        fig.delaxes(ax[i // n_col, i % n_col])
+        # Display the values on top of the bars and highlight the maximum value in red
+        for i, rect in enumerate(plt.gca().containers[0]):
+            value = mean_data[numeric_feat[0]][i]
+            plt.gca().annotate(f'{value:.2f}', 
+                               xy=(rect.get_x() + rect.get_width() / 2, rect.get_height()),
+                               xytext=(0, 3), textcoords='offset points', ha='center', va='bottom',
+                               color='red' if i == max_idx else 'black')
 
-    # Rotate x-axis labels for readability
-    plt.xticks(rotation=45)  
-    plt.tight_layout()
-    plt.show()
+        plt.tight_layout()
+        plt.show()
